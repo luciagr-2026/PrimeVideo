132a0355ddf765bf8bef8fa39420fed4afb98761
luciagr-2026 <luciagroldan@alumnos.cei.es>
2026-06-10 19:11:53 +0200

    Add login page and hero carousel components
    - Created LoginUsers component with associated CSS for styling the login page.
    - Implemented functionality for help section toggle and new account creation button.
    - Added HeroCarousel component to display a carousel of hero images with navigation buttons.
    - Included CSS for the hero carousel layout and styling.
    - Added UpArrow.png asset for the login page.

--- amazon-prime/src/App.jsx
+++ amazon-prime/src/App.jsx
@@ -1,19 +1,41 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import { Introcarousel } from './components/Introcarousel'
 import { HeroCarousel } from './components/HeroCarousel/HeroCarousel'
 import { Header } from './components/Header/Header'
+import { LoginUsers } from './components/Login/Loginusers'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+
+const PrimeContext = createContext()
 
 function App() {
 
 
   return (
-    <>
-  <Header />
-  <Introcarousel />
-  {/* <HeroCarousel /> */}
-    </>
+    <PrimeContext.Provider value={{}}>
+
+      <BrowserRouter>
+        <>
+
+          <Header />
+
+
+          <main>
+            <Routes>
+
+              <Route path='/Login' element={<LoginUsers />} />
+              <Route path='/Introcarousel' element={<Introcarousel />} />
+              <Route path='/Introcarousel' element={<HeroCarousel />} />
+            </Routes>
+
+
+          </main>
+
+
+        </>
+      </BrowserRouter>
+    </PrimeContext.Provider>
   )
 }
 
